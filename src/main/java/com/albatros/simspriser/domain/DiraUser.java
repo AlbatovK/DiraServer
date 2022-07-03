f001ab8ef08cb6ed234d639911fba5e91f689622
AlbatovK <albatovkonstantin@yandex.ru>
2022-07-03 16:06:04 +0600

    :hammer: Refresh

--- src/main/java/com/albatros/simspriser/domain/DiraUser.java
+++ src/main/java/com/albatros/simspriser/domain/DiraUser.java
@@ -14,13 +14,17 @@ public class DiraUser {
 
     private String tokenId;
     private long score = 0;
+    private long scoreOfDay = 0;
+    private long scoreOfWeek = 0;
     private String email;
     private String nickname;
     private List<String> friendsIds = new ArrayList<>();
 
-    public DiraUser(String tokenId, long score, String email, String nickname) {
+    public DiraUser(String tokenId, long score, long scoreOfDay, long scoreOfWeek, String email, String nickname) {
         this.tokenId = tokenId;
         this.score = score;
+        this.scoreOfDay = scoreOfDay;
+        this.scoreOfWeek = scoreOfWeek;
         this.email = email;
         this.nickname = nickname;
     }
